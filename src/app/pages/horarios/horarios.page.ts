ebff6181480402c839fa53c565efd23da834d9f7
cerattobruno <cerattobruno@gmail.com>
2019-11-21 20:06:37 -0300

    Pantalla de reserva.

--- src/app/pages/horarios/horarios.page.ts
+++ src/app/pages/horarios/horarios.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-horarios',
@@ -7,7 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HorariosPage implements OnInit {
 
-  constructor() { }
+  constructor( public alertController: AlertController) {
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar Reserva',
+      message: 'Por favor confirme día y horario seleccionado!',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Confirmar',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
   ngOnInit() {
   }
